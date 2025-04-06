74c03480a29da624b19a2eeef7ca0afa2ebab7da
Your Name <youremail@example.com>
2025-04-06 14:13:51 -0400

    Initial Commit

---
+++ comps/compsApp.swift
@@ -0,0 +1,17 @@
+//
+//  compsApp.swift
+//  comps
+//
+//  Created by Abraham Bloom on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct compsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ compsTests/compsTests.swift
@@ -0,0 +1,16 @@
+//
+//  compsTests.swift
+//  compsTests
+//
+//  Created by Abraham Bloom on 4/6/25.
+//
+
+import Testing
+
+struct compsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
